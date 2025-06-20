aa6caba2da7c3810004b0239e530f0ddde872147
Satyaranjan Baraj <satyaranjanbaral76@gmail.com>
2025-06-21 00:39:51 +0530

    Task

---
+++ june/week2/Task/Task2/Loginpage.js
@@ -0,0 +1,48 @@
+
+document.querySelector("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("iuyt");
+    let username=document.getElementById("user").value;
+    let password=document.getElementById("pass").value;
+
+    let storedPass=localStorage.getItem(username);
+    if(storedPass===null){
+        alert("user not registered");
+    }
+    else{
+        if(storedPass===password){
+            alert("loged in sucessfully");
+            window.location.href='home.html';
+        }
+        else{
+            alert("invalid password");
+            return;
+        }
+    }
+
+
+});
+
+// document.querySelector("form").addEventListener("submit",(e)=>{
+//     e.preventDefault();
+
+
+//      let username=document.getElementById("user").value;
+//     let password=document.getElementById("pass").value;
+
+//     let storedPass=localStorage.getItem(username);
+//     if(storedPass===null){
+//         alert("user not registered");
+//     }
+//     else{
+//         if(storedPass===password){
+//             alert("loged in sucessfully");
+//             window.location.href='home.html';
+//         }
+//         else{
+//             alert("invalid password");
+//             return;
+//         }
+//     }
+
+//     });
